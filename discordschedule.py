9df8a558f10f95e088c20065c9508f077ce1a58b
tunaflsh <bumtuan2001@gmail.com>
2023-12-17 19:05:00 +0100

    Refactor date parsing and move timezones to json

--- discordschedule.py
+++ discordschedule.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import argparse
-import calendar
 import json
 import re
-from datetime import timedelta
 
-import dateutil.parser
-import dateutil.tz
 import pyperclip
+from dateutil.parser import parse as dateparse
+from dateutil.relativedelta import MO
+from dateutil.relativedelta import relativedelta as delta
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -24,7 +23,10 @@ parser = argparse.ArgumentParser(
     '\n    "live format": "- <t:{timestamp}:d> <t:{timestamp}:t> {title}",'
     '\n    "live url format": "- <t:{timestamp}:d> <t:{timestamp}:t> [{title}](<{url}>)",'
     '\n    "stream delimiter": ";",'
-    '\n    "time delimiter": ","'
+    '\n    "time delimiter": ",",'
+    '\n    "timezones": {'
+    '\n        "EST": -5'
+    "\n    }"
     "\n}"
     "\n```"
     "\n"
@@ -73,22 +75,6 @@ parser.add_argument(
 )
 arg = parser.parse_args()
 
-
-url_pattern = re.compile(r"https?://\S+")
-tzinfos = {
-    "JST": +9,
-    "UTC": -0,
-    "GMT": -0,
-    "EDT": -4,
-    "EST": -5,
-    "PDT": -7,
-    "PST": -8,
-}
-tzinfos = {
-    tz: dateutil.tz.tzoffset(tz, offset * 3600) for tz, offset in tzinfos.items()
-}
-
-
 with open(arg.format, "r", encoding="utf-8") as f:
     format = json.load(f)
 
@@ -98,37 +84,28 @@ with open(arg.schedule, "r", encoding="utf-8") as f:
 
 heading = format["heading"]
 date_format = format.get("date format", "%B %d")
-standard_time = format.get("standard time", "12am UTC")
+std_time = format.get("standard time", "12am UTC")
 offline = format.get("offline", "offline")
 live_format = format.get("live format", "<t:{timestamp}> {title}")
 url_format = format.get("live url format", live_format)
 stream_delim = format.get("stream delimiter", ";")
 time_delim = format.get("time delimiter", ",")
+tzinfos = {tz: offset * 3600 for tz, offset in format.get("timezones", {}).items()}
 
+url_pattern = re.compile(r"https?://\S+")
 
-offset = timedelta(days=7 * arg.next_week)
-standard_time = dateutil.parser.parse(standard_time, tzinfos=tzinfos)
-lastday = dateutil.parser.parse("Sun", default=standard_time) + offset
+# monday of this(+arg.next_week) week
+mo = dateparse(std_time, tzinfos=tzinfos) + delta(weeks=arg.next_week, weekday=MO(-1))
+mo, *_, su = weekdays = [mo + delta(weekday=i) for i in range(7)]
 
-
-def parse(*args, **kwargs):
-    date = dateutil.parser.parse(*args, **kwargs) + offset
-    # the parser will select next week if the date is in the past
-    if date > lastday:
-        date -= timedelta(days=7)
-    return date
-
-
-weekdays = [parse(day.name, default=standard_time) for day in calendar.Day]
-# the parser selects the next closest weekday
 schedule = [
     heading.format(
-        startdate=weekdays[0].strftime(date_format),
-        enddate=weekdays[-1].strftime(date_format),
+        startdate=mo.strftime(date_format),
+        enddate=su.strftime(date_format),
     )
 ]
 
-for day_schedule, standard_time in zip(week_schedule, weekdays):
+for day_schedule, std_schedule in zip(week_schedule, weekdays):
     if not day_schedule:
         continue
     if day_schedule == offline:
@@ -136,8 +113,13 @@ for day_schedule, standard_time in zip(week_schedule, weekdays):
         continue
     for stream in day_schedule.split(stream_delim):
         title, *timestamp = stream.split(time_delim, 1)
-        timestamp = parse(timestamp[0], tzinfos=tzinfos) if timestamp else standard_time
-        timestamp = int(timestamp.timestamp())
+        timestamp = int(
+            (
+                dateparse(timestamp[0], tzinfos=tzinfos, default=mo)
+                if timestamp
+                else std_schedule
+            ).timestamp()
+        )
         if match := url_pattern.search(title):
             url = match.group()
             title = title[: match.start()].strip() + title[match.end() :]
